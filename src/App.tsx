dceeea1d997d8c1beec2dc5048f584bb990c36da
youngw77 <103637293+youngw77@users.noreply.github.com>
2023-05-31 14:08:51 +0900

    json data 받아오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import User from './User';
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        setError(null);
-        setUsers([]);
-        setLoading(true);
-        const response = await axios.get(
-          'https://jsonplaceholder.typicode.com/users'
-        );
-        setUsers(response.data);
-      } catch (error:any) {
-        setError(error);
-      }
-      setLoading(false);
-    };
-
-    fetchUsers();
-  }, []);
-
-  type User = {
-    id: number;
-    username: string;
-    name: string;
-  };
-
-  if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
-  if (!users || users.length === 0) return null;
-
-  return (
-    <ul>
-      {users.map((user: User) => (
-        <li key={user.id}>
-          {user.username} ({user.name})
-        </li>
-      ))}
-    </ul>
-  );
+  return <User />;
 }
 
 export default App;
