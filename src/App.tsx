1c7d5ce4b4657c5d36ca34228922480d7c8a04bb
youngw77 <103637293+youngw77@users.noreply.github.com>
2023-06-08 10:46:22 +0900

    BoardCreate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import BoardList from './views/BoardList';
 import BoardSearch from './views/BoardSearch';
 import BoardButton from './views/BoardButton';
--- src/views/BoardCreate.tsx
+++ src/views/BoardCreate.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Editor, EditorTextChangeEvent } from "primereact/editor";
+import { InputText } from "primereact/inputtext";
+import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+import { FileUpload } from 'primereact/fileupload';
 
 const BoardCreate = () => {
-    console.log('render Create');
+    const [text, setText] = useState<string>('');
+    const [value, setValue] = useState<string>('');
+
     return (
-        <div>
-            test
+        <div className="card">
+            <span className="p-float-label">
+                <InputText id="Title" value={value} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)} size={80} />
+                <label htmlFor="Title">Title</label>
+            </span>
+            <h2>Content</h2>
+            <Editor value={text} onTextChange={(e: EditorTextChangeEvent) => setText(e.htmlValue)} style={{ height: '320px' }} />
+            <br />
+            <FileUpload name="demo[]" url={'/api/upload'} multiple accept="image/*" maxFileSize={1000000} emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>} />
+            <br />
+            <Button label="Submit" />
         </div>
     );
 };
