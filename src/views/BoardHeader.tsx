5ba6b2d7f0ef592d008d689c6af6dc005f4a7048
youngw77 <103637293+youngw77@users.noreply.github.com>
2023-06-09 09:42:52 +0900

    BoardList

--- src/views/BoardHeader.tsx
+++ src/views/BoardHeader.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { TabMenu } from "primereact/tabmenu";
 import { MenuItem } from "primereact/menuitem";
 
 const BoardHeader = () => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
   const items: MenuItem[] = [
     { label: "Home", icon: "pi pi-fw pi-home", url: "/" },
     { label: "BoardCreate", icon: "pi pi-fw pi-calendar", url: "/BoardCreate" },
   ];
+  console.log(activeIndex);
 
-  return <TabMenu model={items} />;
+  return <TabMenu model={items} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)} />;
 };
 
 export default BoardHeader;
