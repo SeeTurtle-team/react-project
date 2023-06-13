148f065c98003864126aad52fd63748e16fa49c6
youngw77 <103637293+youngw77@users.noreply.github.com>
2023-06-13 10:25:11 +0900

    Board 추가

--- src/views/BoardCreate.tsx
+++ src/views/BoardCreate.tsx
@@ -2,13 +2,32 @@ import React, { useState } from "react";
 import { Editor, EditorTextChangeEvent } from "primereact/editor";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
-// import { Toast } from "primereact/toast";
 import { FileUpload } from "primereact/fileupload";
 import BoardHeader from "./BoardHeader";
+import axios from "axios";
 
 const BoardCreate = () => {
-  const [text, setText] = useState<string|null>("");
   const [value, setValue] = useState<string>("");
+  const [text, setText] = useState<string>("");
+  // const [url, setUrl] = useState<string>("");
+
+  const handleInputEnter = (e:React.KeyboardEvent<HTMLInputElement>) => {
+
+  }
+  const handleSubmit = () => {
+    console.log("submit");
+    axios.post("/board/create", {
+      title: "test",
+      contents: "test",
+      userId: 5,
+      boardCategoryId: 1,
+    })
+    .then(res => res.data.body)
+    .then(res=>console.log(res));
+
+    setValue("");
+    setText("");
+  }
 
   return (
     <div className="card">
@@ -21,12 +40,14 @@ const BoardCreate = () => {
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             setValue(e.target.value)
           }
+          onKeyDown={handleInputEnter}
           size={80}
         />
         <label htmlFor="Title">Title</label>
       </span>
       <h2>Content</h2>
       <Editor
+        value={text}
         onTextChange={(e: EditorTextChangeEvent) => setText(e.htmlValue)}
         style={{ height: "320px" }}
       />
@@ -42,7 +63,7 @@ const BoardCreate = () => {
         }
       />
       <br />
-      <Button label="Submit" />
+      <Button label="Submit" onClick={handleSubmit}/>
     </div>
   );
 };
