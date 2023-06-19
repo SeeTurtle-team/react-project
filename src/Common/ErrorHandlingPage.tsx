8676012ca1fb0f114e113a25b500e44d71577fbc
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-06-19 14:40:16 +0900

    refactoring

--- src/Common/ErrorHandlingPage.tsx
+++ src/Common/ErrorHandlingPage.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 const ErrorHandlingPage = () => {
+    const { id } = useParams();
+    const [errMsg, setErrMsg] = useState<any>();
+
+    const page429 = () => {
+        return(
+            <div>
+                <h1>현재 서비스 이용이 불가합니다</h1>
+                <h1>잠시 후 다시 시도해주세요</h1>
+            </div>
+        )
+    }
+
+    const errStatueCheck = (id:any) =>{
+        switch(id){
+            case '429':
+                setErrMsg(page429);
+                break;
+        }
+
+    }
+
+    useEffect(() => {
+        errStatueCheck(id)
+    },[])
     return(
         <div>
-            <h1>현재 서비스 이용이 불가합니다</h1>
-            <h1>잠시 후 다시 시도해주세요</h1>
+            {errMsg}
+
         </div>
     )
 }
