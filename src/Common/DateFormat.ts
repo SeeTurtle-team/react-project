b739a5df333bdafc42562001491a898abe0638c4
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-07-04 17:16:38 +0900

    branch test

--- src/Common/DateFormat.ts
+++ src/Common/DateFormat.ts
@@ -1,7 +1,6 @@
 export const dateFormatFunc = (date:any) => {
     const stringToDate = new Date(date);
-    console.log(date)
-    console.log(stringToDate.getDate())
+   
     const boardDate = `${stringToDate.getFullYear()}년 ${stringToDate.getMonth()+1}월 ${stringToDate.getDate()}일`
     return boardDate;
   }
