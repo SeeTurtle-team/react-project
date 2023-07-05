eb57277c65beb158d0c3c129598529c7053f6afd
youngw77 <103637293+youngw77@users.noreply.github.com>
2023-07-05 12:50:41 +0900

    Revert "ReCommit BoardComment"
    This reverts commit 75aacfd933dea05a1bef29d9af695032d856223e.

--- src/views/BoardState.tsx
+++ src/views/BoardState.tsx
@@ -8,10 +8,17 @@ import axios from "axios";
 import { BoardUpdateDto } from "../interface/BoardUpdateDto";
 import { Button } from "primereact/button";
 import { dateFormatFunc } from "../Common/DateFormat";
+import { InputTextarea } from "primereact/inputtextarea";
+import { Fieldset } from "primereact/fieldset";
+import { kMaxLength } from "buffer";
+import { InputText } from "primereact/inputtext";
 import BoardComment from "./BoardComment";
 const BoardState = () => {
   const [board, setBoard] = useState<BoardUpdateDto>();
-
+  const [boardComment, setBoardCommenet] = useState<BoardCommentDto[]>([]);
+  const [comment, setComment] = useState<string>("");
+  const [boardCommentEdit, setBoardCommentEdit] = useState<string>("");
+  const [boardCommentBoolean, setBoardCommentBoolean] = useState<boolean>(false);
   const { index } = useParams();
   const navigate = useNavigate();
 
@@ -24,12 +31,22 @@ const BoardState = () => {
       const errCode = errorHandle(error.response.status);
       navigate(`/ErrorPage/${errCode}`);
     }
+    try {
+      const response = await axios.get("/board/comment/" + index);
+      setBoardCommenet(response.data);
+    } catch (error: any) {
+      console.log(error);
+      const errCode = errorHandle(error.response.status);
+      navigate(`/ErrorPage/${errCode}`);
+    }
   };
 
   useEffect(() => {
     fetchUsers();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  console.log(boardComment);
 
   const handleBoardLike = async () => {
     try {
@@ -47,6 +64,64 @@ const BoardState = () => {
   const boardUpdate = async () => {
     fetchUsers();
   };
+  const handleSubmit = () => {
+    try {
+      axios.post("/board/comment/create", {
+        contents: comment,
+        userId: 5,
+        boardId: Number(index),
+      });
+      setComment("");
+    } catch (error: any) {
+      console.log(error);
+      const errCode = errorHandle(error.response.status);
+      navigate(`/ErrorPage/${errCode}`);
+    }
+  };
+  const handleCommentEdit = (i: number) => {
+    if (boardComment[i].boardComment_isDeleted) {
+      try{
+        axios.patch("/board/comment/update",{
+          id: boardComment[i].boardComment_id,
+          contents: boardCommentEdit,
+          userId: 5,
+          boardId: Number(index)
+        })
+      } catch (error: any) {
+        console.log(error);
+        const errCode = errorHandle(error.response.status);
+        navigate(`/ErrorPage/${errCode}`);
+      }
+      setBoardCommentEdit("");
+      boardComment[i].boardComment_isDeleted = false;
+    } else {
+      boardComment[i].boardComment_isDeleted = true;
+    }
+    setBoardCommentBoolean(!boardCommentBoolean);
+  };
+  const handleCommentDelete = (i: number) => {
+    const userId = 5; // 로그인 후 아이디 값 받아오기
+    try {
+      axios.delete("/board/comment/delete", {
+        data: {
+          id: boardComment[i].boardComment_id,
+          userId: userId,
+        },
+      });
+      if (userId === boardComment[i].userId) {
+        alert("댓글이 삭제 되었습니다.");
+      } else {
+        alert("댓글 작성자만 삭제할 수 있습니다.");
+      }
+    } catch (error: any) {
+      console.log(error);
+      const errCode = errorHandle(error.response.status);
+      navigate(`/ErrorPage/${errCode}`);
+    }
+  };
+  const handleInputEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+
+
 
   return (
     <div className="card">
@@ -76,7 +151,54 @@ const BoardState = () => {
       <div style={{ marginBottom: "1rem" }}>
         <Button label="Update" onClick={boardUpdate}></Button>
       </div>
-      <BoardComment index={index}/>
+      {boardComment.map((comment, i) => (
+        <Fieldset key={i}>
+          <div style={{ marginBottom: "1rem" }}>
+            아이디:{comment.user_nickname} <br />
+            내용:
+            {boardComment[i].boardComment_isDeleted === false ? (
+              comment.boardComment_contents
+            ) : (
+              <InputText 
+                value={boardCommentEdit}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setBoardCommentEdit(e.target.value)}
+                onKeyDown={handleInputEnter}
+                placeholder={boardComment[i].boardComment_contents}
+              />
+            )}
+            <br />
+            작성일:{String(comment.boardComment_dateTime)}
+          </div>
+          <div>
+            <Button
+              label="수정"
+              severity="warning"
+              icon="pi pi-pencil"
+              style={{ marginRight: "1rem" }}
+              onClick={() => handleCommentEdit(i)}
+            />
+            <Button
+              label="삭제"
+              severity="danger"
+              icon="pi pi-trash"
+              onClick={() => handleCommentDelete(i)}
+            />
+          </div>
+        </Fieldset>
+      ))}
+      <div style={{ marginBottom: "1rem" }}>
+        <span style={{ marginRight: "1rem" }}>
+          <InputTextarea
+            rows={3}
+            cols={50}
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </span>
+        <span>
+          <Button label="댓글 작성" onClick={handleSubmit} />
+        </span>
+      </div>
     </div>
   );
 };
