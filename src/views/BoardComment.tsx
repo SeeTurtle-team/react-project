3419591eb45e35aa92e6b5cd18bb98c2b4e512b9
youngw77 <103637293+youngw77@users.noreply.github.com>
2023-07-06 10:06:47 +0900

    BoardCommentBan Modal 구현

--- src/views/BoardComment.tsx
+++ src/views/BoardComment.tsx
@@ -1,19 +1,37 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { errorHandle } from '../Common/ErrorHandle';
+import Modal from 'react-modal';
+
 import { useNavigate } from "react-router-dom"; // Import the useHistory hook
 import { Fieldset } from 'primereact/fieldset';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { InputTextarea } from 'primereact/inputtextarea';
+
 import { BoardCommentDto } from '../interface/BoardComment.Dto';
+
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
 
 const BoardComment = ({index}:any) => {
     const [boardComment, setBoardCommenet] = useState<BoardCommentDto[]>([]);
     const [comment, setComment] = useState<string>("");
     const [boardCommentEdit, setBoardCommentEdit] = useState<string>("");
     const [boardCommentBoolean, setBoardCommentBoolean] = useState<boolean>(false);
+    const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+    const [banReason, setBanReason] = useState<string>("");
     const navigate = useNavigate();
+    const userId = 5; // 로그인 후 아이디 값 받아오기
+
 
     const fetchUsers = async () => {
     try {
@@ -35,7 +53,7 @@ const BoardComment = ({index}:any) => {
         try {
           axios.post("/board/comment/create", {
             contents: comment,
-            userId: 5,
+            userId: userId,
             boardId: Number(index),
           });
           setComment("");
@@ -51,7 +69,7 @@ const BoardComment = ({index}:any) => {
             axios.patch("/board/comment/update",{
               id: boardComment[i].boardComment_id,
               contents: boardCommentEdit,
-              userId: 5,
+              userId: userId,
               boardId: Number(index)
             })
           } catch (error: any) {
@@ -69,7 +87,6 @@ const BoardComment = ({index}:any) => {
 
 
     const handleCommentDelete = (i: number) => {
-        const userId = 5; // 로그인 후 아이디 값 받아오기
         try {
           axios.delete("/board/comment/delete", {
             data: {
@@ -91,6 +108,26 @@ const BoardComment = ({index}:any) => {
       const handleInputEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
     
       };
+      const handleCommentBan = (i: number) => {
+        setModalIsOpen(!modalIsOpen);
+      }
+      const handleCommentBanSend = () => {
+        try{
+            axios.post("/board/notify", {
+                reason: banReason,
+                boardId: Number(index),
+                userId: userId
+            })
+        } catch (error: any) {
+          console.log(error);
+          const errCode = errorHandle(error.response.status);
+          navigate(`/ErrorPage/${errCode}`);
+        }
+        setModalIsOpen(false);
+      }
+      const closeModal = () => {
+        setModalIsOpen(false);
+      }
 
     return (
         <div>
@@ -124,8 +161,34 @@ const BoardComment = ({index}:any) => {
               label="삭제"
               severity="danger"
               icon="pi pi-trash"
+              style={{ marginRight: "1rem" }}
               onClick={() => handleCommentDelete(i)}
             />
+            <Button
+              label="신고"
+              severity="danger"
+              icon="pi pi-ban"
+              onClick={() => handleCommentBan(i)}
+            />
+            <Modal 
+            isOpen={modalIsOpen} 
+            appElement={document.getElementById('root') as HTMLElement}
+            style={customStyles}
+            >
+            <h2>신고 이유</h2>
+            <InputTextarea id="userid" keyfilter="alphanum" style={{width:'12rem', marginBottom: "1rem" }} className="w-full" value={banReason} onChange={(e) => setBanReason(e.target.value)} rows={5} cols={40} />
+            <div className="flex flex-wrap justify-content-center gap-5">
+            <Button
+            label='신고'
+            onClick={handleCommentBanSend}
+            />
+            <Button
+            label='취소'
+            severity='danger'
+            onClick={closeModal}
+            />
+            </div>
+            </Modal>
           </div>
         </Fieldset>
       ))}
