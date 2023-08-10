8a1512fe09ba7c421f593f304b1541ed0ee07b45
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-08-10 11:34:37 +0900

    add room

--- src/views/SmallTalk/SmallTalk.tsx
+++ src/views/SmallTalk/SmallTalk.tsx
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
-import { SmallChatDto } from "../../interface/SmallChatDto";
+import { SmallTalkDto } from "../../interface/SmallTalkDto";
 import { ChangeEvent, FormEvent, useCallback, useEffect, useRef, useState } from "react";
+import { useParams } from "react-router";
 
 const socket = io('http://localhost:5000/chat');
 
@@ -37,13 +38,14 @@ const test = {
 }
 
 const SmallTalk = () => {
-  const [chats, setChats] = useState<SmallChatDto[]>([]);
-  const [message, setMessage] = useState<String>('');
+  const [chats, setChats] = useState<SmallTalkDto[]>([]);
+  const [message, setMessage] = useState<string>('');
   const chatContainerEl = useRef<HTMLDivElement>(null);
+  const {roomId} = useParams();
 
   const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
-  }, []);
+  }, []); 
 
   // 채팅이 길어지면(chats.length) 스크롤이 생성되므로, 스크롤의 위치를 최근 메시지에 위치시키기 위함
   useEffect(() => {
@@ -62,11 +64,11 @@ const SmallTalk = () => {
 
     // message event listener
     useEffect(() => {
-      const messageHandler = (chat: SmallChatDto) =>
+      const messageHandler = (chat: SmallTalkDto) =>
         setChats((prevChats) => [...prevChats, chat]);
       socket.on('message', messageHandler);
-  
-  
+      
+      socket.emit('join-room',roomId);
       return () => {
         socket.off('message', messageHandler);
       };
@@ -81,12 +83,12 @@ const SmallTalk = () => {
       if (!message) return alert('메시지를 입력해 주세요.');
 
       console.log(message);
-      socket.emit('message', message, (chat: SmallChatDto) => {
+      socket.emit('message', { roomId, message }, (chat: SmallTalkDto) => {
         setChats((prevChats) => [...prevChats, chat]);
         setMessage('');
       });
     },
-    [message]
+    [message,roomId]
   );
 
   return (
@@ -94,7 +96,7 @@ const SmallTalk = () => {
       <div>
         {chats.map((chat,index) => (
           <div>
-            {chat.userId} : {chat.smallChat}
+            {chat.userId} : {chat.contents}
           </div>
         ))}
       </div>
@@ -102,7 +104,7 @@ const SmallTalk = () => {
 
 
       <form method="submit" onSubmit={onSendMessage}>
-        <input type='text' onChange={onChange} />
+        <input type='text' value={message} onChange={onChange} />
         <input type="submit" value="Submit" />
 
       </form>
