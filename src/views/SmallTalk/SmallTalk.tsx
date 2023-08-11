7b918504997981ec0bf6c680b24c20394fe8df7e
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-08-11 15:49:38 +0900

    ui update

--- src/views/SmallTalk/SmallTalk.tsx
+++ src/views/SmallTalk/SmallTalk.tsx
@@ -4,55 +4,26 @@ import { ChangeEvent, FormEvent, useCallback, useEffect, useRef, useState } from
 import { useParams } from "react-router";
 import { useCookies } from "react-cookie";
 import axios from "axios";
+import { Card } from 'primereact/card';import { Divider } from "primereact/divider";
+;
 
 const socket = io('http://localhost:5000/chat');
 
-const test = {
-  getlist() {
-    return [
-      {
-        userId:1,
-        userName:'dasfas',
-        smallChat:'dsafsadf'
-      },
-      {
-        userId:2,
-        userName:'dasfas',
-        smallChat:'dsafsadf'
-      },
-      {
-        userId:3,
-        userName:'dasfas',
-        smallChat:'dsafsadf'
-      },
-      {
-        userId:4,
-        userName:'dasfas',
-        smallChat:'dsafsadf'
-      },
-      {
-        userId:5,
-        userName:'dasfas',
-        smallChat:'dsafsadf'
-      },
-    ]
-  }
-}
 
 const SmallTalk = () => {
   const [chats, setChats] = useState<SmallTalkDto[]>([]);
   const [message, setMessage] = useState<string>('');
   const chatContainerEl = useRef<HTMLDivElement>(null);
-  const {roomId} = useParams();
+  const { roomId } = useParams();
 
 
   const [cookies, setCookie, removeCookie] = useCookies(["id"]);
   const accessToken = cookies.id;
-  const headers = {Authorization:'Bearer '+accessToken}
+  const headers = { Authorization: 'Bearer ' + accessToken }
 
   const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
-  }, []); 
+  }, []);
 
   // 채팅이 길어지면(chats.length) 스크롤이 생성되므로, 스크롤의 위치를 최근 메시지에 위치시키기 위함
   useEffect(() => {
@@ -69,27 +40,27 @@ const SmallTalk = () => {
   }, [chats.length]);
 
 
-    // message event listener
-    useEffect(() => {
-      getList();
-      const messageHandler = (chat: SmallTalkDto) =>
-        setChats((prevChats) => [...prevChats, chat]);
-      socket.on('message', messageHandler);
-      
-      socket.emit('join-room',roomId);
-      return () => {
-        socket.off('message', messageHandler);
-      };
+  // message event listener
+  useEffect(() => {
+    getList();
+    const messageHandler = (chat: SmallTalkDto) =>
+      setChats((prevChats) => [...prevChats, chat]);
+    socket.on('message', messageHandler);
 
-      //setChats(test.getlist());
-    }, []);
+    socket.emit('join-room', roomId);
+    return () => {
+      socket.off('message', messageHandler);
+    };
+
+    //setChats(test.getlist());
+  }, []);
 
 
   const getList = async () => {
-    const response = await axios(`/small-talk/getSmallTalk/${roomId}`,{headers});
+    const response = await axios(`/small-talk/getSmallTalk/${roomId}`, { headers });
     setChats(response.data)
 
-  }  
+  }
 
   const onSendMessage = useCallback(
     (e: FormEvent<HTMLFormElement>) => {
@@ -103,20 +74,28 @@ const SmallTalk = () => {
         setMessage('');
       });
     },
-    [message,roomId]
+    [message, roomId]
   );
 
   return (
     <div className="card">
       <div>
-        {chats.map((chat,index) => (
-          <div>
-            {chat.userName} : {chat.contents}
+        {chats.map((chat, index) => (
+          <div className='chatBox' style={{marginTop:'0.5rem', border:'solid 0.1px',borderRadius:'10px', background:'#f4f5f5'}}>
+            <h3 style={{marginLeft:'1rem'}}>{chat.userName}의 의견</h3>
+            <Divider/>
+            <p style={{marginLeft:'1rem'}} >
+              {chat.contents}
+            </p>
           </div>
+
+
+
+
         ))}
       </div>
 
-
+      <br/>
 
       <form method="submit" onSubmit={onSendMessage}>
         <input type='text' value={message} onChange={onChange} />
