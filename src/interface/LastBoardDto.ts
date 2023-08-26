8dc079bbd26c8d2757e1c21637cdfa874314bf03
chobkyu <qudqud97@naver.com>
2023-08-27 02:26:27 +0900

    1dan

---
+++ src/interface/LastBoardDto.ts
@@ -0,0 +1,11 @@
+export interface LastBoardDto{
+    id:number;
+    title:string;
+    contents:string;
+    dateTime :Date;
+    isDeleted:boolean;
+    isModified:boolean;
+    ban:boolean;
+    recommend:number;
+}
+
--- src/views/Index/LastBoard.tsx
+++ src/views/Index/LastBoard.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+//import { LastBoardDto } from "../../interface/lastBoardDto";
+import { useNavigate } from "react-router-dom";
 
 //최근 업로드 게시판 불러오기
 const LastBoard = () => {
+
+    //const [board,setBoard] = useState<LastBoardDto>();
+    const navigate = useNavigate();
+
+    
+
     return (
         <>
         </>
