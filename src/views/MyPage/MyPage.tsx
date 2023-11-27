32f45007eb9f677a731b34312c4ace2d87c59018
choikiyeop <cky9709@naver.com>
2023-11-27 22:28:46 +0900

    profile image 업로드 구현

--- src/views/MyPage/MyPage.tsx
+++ src/views/MyPage/MyPage.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { TabPanel, TabPanelPassThroughOptions, TabView } from 'primereact/tabview';
 import { Avatar } from 'primereact/avatar';
 import { Image } from "primereact/image";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router";
 import { Button } from "primereact/button";
@@ -10,8 +10,11 @@ import { GetEbookListDto } from "../../interface/GetEbookListDto";
 import { Board } from "../../interface/BoardListDto";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { User } from "../../interface/UserAddInfo";
+import { Dialog } from "primereact/dialog";
+import { HandleFileInput } from "../../Common/S3ImgUpload";
+import { errorHandle } from "../../Common/ErrorHandle";
 
 const tabPanelPt: TabPanelPassThroughOptions = { 
   root: {
@@ -30,6 +33,21 @@ const NoData = () => <div style={{ display: "flex", justifyContent: "center", al
 const MyPage = () => {
   const navigate = useNavigate();
   const [cookies, setCookies, removeCookie] = useCookies(["id"]);
+  const [visible, setVisible] = useState<boolean>(false);
+  const [SubImgUrl, setSubImgUrl] = useState<string>('');
+  axios.defaults.baseURL = "http://localhost:5000";
+  const { data: userImage } = useQuery<string>({
+    queryKey: ['userImage'],
+    queryFn: () => 
+      axios
+      .get(`http://localhost:5000/user/url`, { 
+        headers: {
+          Authorization: `Bearer ${cookies.id}`
+        }
+      })
+      .then(res => res.data.data)
+      .catch(err => console.log(err))
+  })
   const { data: userInfo } = useQuery<User>({
     queryKey: ["userInfo"],
     queryFn: () => 
@@ -91,108 +109,133 @@ const MyPage = () => {
         .catch(err => console.log(err))
   })
 
+  const { mutate } = useMutation({
+    mutationFn: (url: string) =>
+      axios
+        .post(`http://localhost:5000/user/url`, { url: url }, {
+          headers: { Authorization: `Bearer ${cookies.id}`}
+        })
+        .then(res => res)
+        .catch(err => console.log(err)),
+    onSuccess: () => {
+      setVisible(false);
+    }
+  })
+
+  const getImgUrl = async (e: any) => {
+    const imgurl = await HandleFileInput(e, { Authorization: `Bearer ${cookies.id}` });
+    if (imgurl.success === false) {
+      const errCode = errorHandle(imgurl.msg);
+      navigate(`/ErrorPage/${errCode}`);
+    } else {
+      setSubImgUrl(imgurl);
+    }
+  }
+
   const logout = () => {
     removeCookie("id");
     alert("로그아웃 되었습니다");
+    navigate("/");
   }
 
-  useEffect(() => {
-    axios
-    .get(`http://localhost:5000/user/s3url`, { 
-      headers: {
-        Authorization: `Bearer ${cookies.id}`
-      }
-    })
-    .then(res => console.log(res.data))
-    .catch(err => console.log(err))
-  }, [cookies.id])
-
   return (
-    <div className="card" style={{ width:'60%', justifyContent:'center', marginLeft:'20%' }}>
-      <div className="mt-4 mb-4">
-        <div className="flex text-gray-900">
-          <Avatar image={"https://www.gravatar.com/avatar/05dfd4b41340d09cae045235eb0893c3?d=mp"} size="xlarge" shape="circle" icon="pi pi-user" className="mr-2 flex h-full" style={{ alignSelf: "center" }}/>
-          <div className="mx-2">
-            <div className="flex">
-              <h2 className="m-0">{userInfo?.nickname}</h2>
-              <Button className="pi pi-sign-out" color="#fff" text onClick={() => {
-                logout();
-                console.log("userInfo", userInfo);
-                console.log("ebookHistoryList", ebookHistoryList)
-                console.log("ebookList", ebookList);
-                console.log("likedBoardList", likedBoardList);
-                console.log("board", boardList);
-              }}></Button>
+    <>
+      <Dialog visible={visible} onHide={() => setVisible(false)} style={{ width: '40vw' }} maximizable contentStyle={{ height: '300px'}}>
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+          <Avatar 
+            image={userImage}
+            size="xlarge"
+            shape="circle"
+          />
+          <input type="file" onChange={(e) => getImgUrl(e)} />
+          <button onClick={() => mutate(SubImgUrl)}>저장</button>
+        </div>
+      </Dialog>
+      
+      <div className="card" style={{ width:'60%', justifyContent:'center', marginLeft:'20%' }}>
+        <div className="mt-4 mb-4">
+          <div className="flex text-gray-900">
+            <Avatar 
+              image={userImage} 
+              size="xlarge" 
+              shape="circle" 
+              className="mr-2 flex" 
+              style={{ alignSelf: "center" }} 
+              onClick={() => setVisible(true)}
+            />
+            <div className="mx-2">
+              <div className="flex">
+                <h2 className="m-0">{userInfo?.nickname}</h2>
+                <Button className="pi pi-sign-out" color="#fff" text onClick={() => logout()} />
+              </div>
+              <div>{userInfo?.userId}</div>
             </div>
-            <div>{userInfo?.userId}</div>
           </div>
         </div>
-        
+
+        <TabView>
+          <TabPanel header="내가 열람한 ebook" pt={tabPanelPt} contentStyle={{ display: "flex" }}>
+            {!ebookHistoryList?.length && <NoData />}
+            {ebookHistoryList?.map(ebookHistory => (
+              <div key={ebookHistory.id} style={{ width: 150, height: 150, border: "1px solid lightgray", display: "flex", justifyContent: "center", alignItems: "center", marginRight: "8px" }}>
+                {ebookHistory.coalesce === "noUrl" ?(
+                  <span key={ebookHistory.id}>No Image</span>
+                ):(
+                  <Image key={ebookHistory.id} src={ebookHistory.coalesce} width="200"></Image>
+                )}
+              </div>
+            ))}
+          </TabPanel>
+          <TabPanel header="내가 작성한 ebook" pt={tabPanelPt} contentStyle={{ display: "flex" }}>
+            {!ebookList?.length && <NoData />}
+            {ebookList?.map(ebook => (
+              <div style={{ width: 150, height: 150, border: "1px solid lightgray", display: "flex", justifyContent: "center", alignItems: "center", marginRight: "8px" }}>
+                {ebook.coalesce === "noUrl" ?(
+                  <span key={ebook.id}>No Image</span>
+                ):(
+                  <Image key={ebook.id} src={ebook.coalesce} width="200"></Image>
+                )}
+              </div>
+            ))}
+          </TabPanel>
+        </TabView>
+
+        <TabView>
+          <TabPanel header="내가 추천한 게시글" pt={tabPanelPt}>
+            <DataTable
+              value={likedBoardList}
+              tableStyle={{ minWidth: "50rem" }}
+              paginator
+              rows={10}
+              onRowClick={(e) => navigate(`/BoardState/${e.data.id}`)}
+            >
+              <Column field="id" header="ID" />
+              <Column field="title" header="Title" />
+              <Column field="category" header="Category" />
+              <Column field="nickname" header="Nickname" />
+              <Column field="recommendCount" header="Recommend Count" />
+              <Column field="dateTime" header="Time" />
+            </DataTable>
+          </TabPanel>
+          <TabPanel header="내가 작성한 게시글" pt={tabPanelPt} >
+            <DataTable
+              value={boardList}
+              tableStyle={{ minWidth: "50rem" }}
+              paginator
+              rows={10}
+              onRowClick={(e) => navigate(`/BoardState/${e.data.id}`)}
+            >
+              <Column field="id" header="ID" />
+              <Column field="title" header="Title" />
+              <Column field="category" header="Category" />
+              <Column field="nickname" header="Nickname" />
+              <Column field="dateTime" header="Time" />
+            </DataTable>
+          </TabPanel>
+        </TabView>
+
       </div>
-
-      <TabView>
-        <TabPanel header="내가 열람한 ebook" pt={tabPanelPt} contentStyle={{ display: "flex" }}>
-          
-          {!ebookHistoryList?.length && <NoData />}
-          {ebookHistoryList?.map(ebookHistory => (
-            <div style={{ width: 150, height: 150, border: "1px solid lightgray", display: "flex", justifyContent: "center", alignItems: "center", marginRight: "8px" }}>
-              {ebookHistory.coalesce === "noUrl" ?(
-                <span key={ebookHistory.id}>No Image</span>
-              ):(
-                <Image key={ebookHistory.id} src={ebookHistory.coalesce} width="200"></Image>
-              )}
-            </div>
-          ))}
-        </TabPanel>
-        <TabPanel header="내가 작성한 ebook" pt={tabPanelPt} contentStyle={{ display: "flex" }}>
-          {!ebookList?.length && <NoData />}
-          {ebookList?.map(ebook => (
-            <div style={{ width: 150, height: 150, border: "1px solid lightgray", display: "flex", justifyContent: "center", alignItems: "center", marginRight: "8px" }}>
-              {ebook.coalesce === "noUrl" ?(
-                <span key={ebook.id}>No Image</span>
-              ):(
-                <Image key={ebook.id} src={ebook.coalesce} width="200"></Image>
-              )}
-            </div>
-          ))}
-        </TabPanel>
-      </TabView>
-
-      <TabView>
-        <TabPanel header="내가 추천한 게시글" pt={tabPanelPt}>
-          <DataTable
-            value={likedBoardList}
-            tableStyle={{ minWidth: "50rem" }}
-            paginator
-            rows={10}
-            onRowClick={(e) => navigate(`/BoardState/${e.data.id}`)}
-          >
-            <Column field="id" header="ID" />
-            <Column field="title" header="Title" />
-            <Column field="category" header="Category" />
-            <Column field="nickname" header="Nickname" />
-            <Column field="recommendCount" header="Recommend Count" />
-            <Column field="dateTime" header="Time" />
-          </DataTable>
-        </TabPanel>
-        <TabPanel header="내가 작성한 게시글" pt={tabPanelPt} >
-          <DataTable
-            value={boardList}
-            tableStyle={{ minWidth: "50rem" }}
-            paginator
-            rows={10}
-            onRowClick={(e) => navigate(`/BoardState/${e.data.id}`)}
-          >
-            <Column field="id" header="ID" />
-            <Column field="title" header="Title" />
-            <Column field="category" header="Category" />
-            <Column field="nickname" header="Nickname" />
-            <Column field="dateTime" header="Time" />
-          </DataTable>
-        </TabPanel>
-      </TabView>
-
-    </div>
+    </>
   )
 }
 
